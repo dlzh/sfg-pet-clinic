419f878bea9bb46566b898c3d1f501761d8f8233
Donglai <dlzhangster@gmail.com>
2020-06-01 23:27:06 +0930

    CrudService

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/CrudService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/CrudService.java
@@ -5,15 +5,26 @@ import java.util.Set;
 /**
  * Created by jt on 7/21/18.
  */
-public interface CrudService<T, ID> {
-
+//public interface CrudService<T, ID> {
+//
+//    Set<T> findAll();
+//
+//    T findById(ID id);
+//
+//    T save(T object);
+//
+//    void delete(T object);
+//
+//    void deleteById(ID id);
+//}
+public interface CrudService <T, ID>{
     Set<T> findAll();
 
-    T findById(ID id);
+    T findById (ID id);
 
     T save(T object);
 
     void delete(T object);
 
-    void deleteById(ID id);
+    void deleteById( ID id);
 }
